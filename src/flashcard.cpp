b5c8a2f1b8c9dae9ee66bcbd5ccd01f9aedf902a
Sihao Chen <fchen0217@gmail.com>
2017-02-21 18:10:05 -0600

    init project

---
+++ src/flashcard.cpp
@@ -0,0 +1,18 @@
+/*
+ * flashcard.cpp
+ *
+ *  Created on: Feb 21, 2017
+ *      Author: fchen
+ */
+
+#include "flashcard.h"
+
+flashcard::flashcard() {
+	// TODO Auto-generated constructor stub
+
+}
+
+flashcard::~flashcard() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/flashcard.h
@@ -0,0 +1,22 @@
+/*
+ * flashcard.h
+ *
+ *  Created on: Feb 21, 2017
+ *      Author: fchen
+ */
+
+#ifndef FLASHCARD_H_
+#define FLASHCARD_H_
+
+#include
+class flashcard {
+public:
+	flashcard();
+	virtual ~flashcard();
+
+private:
+
+};
+
+
+#endif /* FLASHCARD_H_ */
